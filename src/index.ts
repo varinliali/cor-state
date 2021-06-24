57cc204d82e72efb07f57c7ba8eb43a14a2c140c
varinliali <avarinlioglu@gmail.com>
2021-06-24 18:53:23 +0300

    api support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export { default as initCor } from './initCor'
-export { default as useCor } from './useCor'
-export { default as useCorContained } from './useCorContained'
-export { default as useCorDependend } from './useCorDependend'
+export { default as useCor } from './useCor';
--- src/useCor.ts
+++ src/useCor.ts
@@ -5,14 +5,18 @@ declare global {
     Cor: any;
   }
 }
-type ChangeKey = string
+type ChangeKey = string;
 type TCorProps = {
   keys: ChangeKey[];
   onChange?: (keys: ChangeKey[]) => void;
   defaultValues?: any;
-}
+};
 
-export default function useCor({ keys = [], onChange, defaultValues }: TCorProps) {
+export default function useCor({
+  keys = [],
+  onChange,
+  defaultValues,
+}: TCorProps) {
   const [flag, triggerRerender] = useState(false);
 
   const set = useRef(false);
@@ -27,43 +31,68 @@ export default function useCor({ keys = [], onChange, defaultValues }: TCorProps
           window.Cor[key] = defaultValues[key];
         }
       }
-      triggerRerender((prev) => !prev);
+      triggerRerender(prev => !prev);
     }
 
     window.addEventListener('Cor', onMessageReceived);
-    return () => window.removeEventListener("Cor", onMessageReceived)
+    return () => window.removeEventListener('Cor', onMessageReceived);
   }, []);
 
-
-
   function onMessageReceived({ detail }: any) {
-    if (!detail) return;
+    if (!detail || detail.length === 0) return;
 
-    if (keys.some((key) => detail.includes(key))) {
-      triggerRerender((prev) => !prev);
+    if (checkIfIncludes(keys, detail)) {
+      triggerRerender(prev => !prev);
       onChange && onChange(detail);
     }
   }
 
-  function c(action: string | any) {
-    if (typeof action === 'string') {
-      return window.Cor[action]
-    } else {
-      try {
-        const keys = Object.keys(action)
-        if (!keys || keys.length === 0) return
-        for (const key of keys) {
-          window.Cor[key] = action[key]
-        }
-
-        const corEvent = new CustomEvent('Cor', { detail: keys });
-        window.dispatchEvent(corEvent);
-      }
-      catch (error) {
-        console.log("error setting key", { action, error })
-      }
-    }
-  }
-
   return { flag, c };
+}
+
+function c(action: string | any, fetcher?: any, skip?: boolean) {
+  if (typeof action === 'string') {
+    const data = getFromWindow(action);
+    if (data !== undefined) return data;
+    else if (!fetcher) return;
+    else {
+      if (skip) return;
+      setToWindow({ [action]: 'loading' });
+      fetcher()
+        .then((res: any) => res.json())
+        .then((data: any) => setToWindow({ [action]: data }));
+      return 'loading';
+    }
+  } else {
+    setToWindow(action);
+  }
+}
+
+function getFromWindow(action: string) {
+  return window.Cor[action];
+}
+
+function setToWindow(action: any) {
+  try {
+    const keys = Object.keys(action);
+    if (!keys || keys.length === 0) return;
+    for (const key of keys) {
+      window.Cor[key] = action[key];
+    }
+
+    const corEvent = new CustomEvent('Cor', { detail: keys });
+    window.dispatchEvent(corEvent);
+  } catch (error) {
+    console.log('error setting key', { action, error });
+  }
+}
+function checkIfIncludes(keys: string[], detail: string[]) {
+  return keys.some(key => checkDetailIncludes(detail, key));
+}
+function checkDetailIncludes(detail: string[], key: string) {
+  const splitWildCard = key.split('*');
+  const hasWildCard = splitWildCard?.length > 0;
+  return hasWildCard
+    ? detail.some(key => key.startsWith(splitWildCard[0]))
+    : detail.includes(key);
 }
